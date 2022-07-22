899d73632f1ca4eaff6668ee64817b60fa39f93f
rtk18 <mehra.ritik18@gmail.com>
2022-07-22 15:52:00 +0530

    added swagger documentaion

--- routes/tea.js
+++ routes/tea.js
@@ -2,6 +2,22 @@ const express = require('express')
 const router = express.Router()
 const teaController = require('../controllers/tea')
 
+/**
+ * @swagger
+ * /api/tea:
+ *   get:
+ *     summary: Returns all posts
+ *     tags: [Posts]
+ *     responses:
+ *       200:
+ *         description: the list of the posts
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Post'
+ */
 router.get('/tea', teaController.getAllTea)
 router.post('/tea', teaController.newTea)
 router.delete('/tea', teaController.deleteAllTea)
--- server.js
+++ server.js
@@ -1,11 +1,38 @@
 const express = require('express')
 const routes = require('./routes/tea') // import the routes
+import swaggerUI from 'swagger-ui-express'
+import swaggerJsDoc from 'swagger-jsdoc'
 
 const app = express()
 
 app.use(express.json())
 
-app.use('/', routes) //to use the routes
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Book Store Application API Documentation',
+            version: '1.0.0',
+            contact: {
+                name: 'Ritik Mehra',
+                email: 'mehra.ritik18@gmail.com',
+            },
+        },
+
+        servers: [
+            {
+                url: 'http://localhost:4000',
+                description: 'Book Store Application API Documentation',
+            },
+        ],
+    },
+    apis: ['./Routes/*.js'],
+}
+
+const specs = swaggerJsDoc(options)
+app.use('/swagger', swaggerUI.serve, swaggerUI.setup(specs))
+
+app.use('/api', routes) //to use the routes
 
 const listener = app.listen(process.env.PORT || 4000, () => {
     console.log('Your app is listening on port ' + listener.address().port)
