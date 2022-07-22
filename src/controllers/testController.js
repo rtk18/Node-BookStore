34bce179f6f74c52e056a394fe6a53dd5cc9d4db
rtk18 <mehra.ritik18@gmail.com>
2022-07-23 00:18:06 +0530

    db string change

--- src/controllers/testController.js
+++ src/controllers/testController.js
@@ -1,6 +1,7 @@
 const { Client } = require('pg')
 const client = new Client({
     connectionString:
+        process.env.DATABASE_URL ||
         'postgres://rkykudmnzolvex:df6fdf2cfc06fcabc66cd99af9d0985bf17846733826d5b87c98f9022698ba3b@ec2-18-214-35-70.compute-1.amazonaws.com:5432/df1nvsvjsu6o78',
     ssl: {
         rejectUnauthorized: false,
