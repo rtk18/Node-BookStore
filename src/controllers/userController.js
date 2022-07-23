06958d6761a92c78b12fa8c1a6bfe65c04ae4728
rtk18 <mehra.ritik18@gmail.com>
2022-07-23 22:16:23 +0530

    added adb connection to .env file

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,9 +1,10 @@
+const config = require('dotenv').config()
+
 const { Client } = require('pg')
 const client = new Client({
     connectionString:
-        process.env.DATABASE_URL ||
-        'postgres://rkykudmnzolvex:df6fdf2cfc06fcabc66cd99af9d0985bf17846733826d5b87c98f9022698ba3b@ec2-18-214-35-70.compute-1.amazonaws.com:5432/df1nvsvjsu6o78',
-    ssl: {
+        process.env.DATABASE_URL, 
+            ssl: {
         rejectUnauthorized: false,
     },
 })
